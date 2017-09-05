57ba4f54bd989e1fafb122aa213f0c9007c86b1a
Frederic Defays <frederic.defays@gmail.com>
2017-09-05 11:50:40 +0200

    Js - Remettre tous les paragraphes à la même hauteur

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log('hello world !');
+
+$( document ).ready(function(){
+  var max = 0;
+	$(".drink .product-drink p").each(function( index ){
+		if ($(this).height() > max) {
+			max = $(this).height();
+		}
+	})
+	$(".drink .product-drink p").height(max);
+});
